40437bce125b77eee7e3f21193de134654cc9a08
Marta <martadabrowka@gmail.com>
2017-11-13 19:35:05 +0000

    wrote onCreate method

--- app/src/main/java/com/example/marta/wordcounter/MainActivity.java
+++ app/src/main/java/com/example/marta/wordcounter/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.marta.wordcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView wordCounter;
+    private EditText inputText;
+    private Button countButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.wordCounter = (TextView) findViewById(R.id.counter);
+        this.inputText = (EditText) findViewById(R.id.inputText);
+        this.countButton = (Button) findViewById(R.id.wordCount);
     }
+
+
 
 
 }
