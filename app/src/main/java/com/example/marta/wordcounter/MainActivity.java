3d7062e40ce26e6fa8ab6f842a1462b9ccb62bee
Marta <martadabrowka@gmail.com>
2017-11-13 19:46:43 +0000

    added onButtonClicked method

--- app/src/main/java/com/example/marta/wordcounter/MainActivity.java
+++ app/src/main/java/com/example/marta/wordcounter/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.marta.wordcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView wordCounter;
+    private TextView counter;
     private EditText inputText;
     private Button countButton;
 
@@ -16,12 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.wordCounter = (TextView) findViewById(R.id.counter);
+        this.counter = (TextView) findViewById(R.id.counter);
         this.inputText = (EditText) findViewById(R.id.inputText);
-        this.countButton = (Button) findViewById(R.id.wordCount);
+        this.countButton = (Button) findViewById(R.id.word_count);
+        Log.d("MainActivity", "OnCreate called");
     }
 
 
+    public void onButtonClicked(View button) {
+        Log.d("MainActivity", "onButtonClicked");
+        String text = this.inputText.getText().toString();
+        Log.d(getClass().toString(), text);
+        WordCounter calculator = new WordCounter();
+        int answer = calculator.countWords(text);
+        this.counter.setText(answer);
+
+    }
 
 
 }
+
+
+
+
